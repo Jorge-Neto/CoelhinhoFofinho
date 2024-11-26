d11a7b6251fc7f9bc247a5f885da5b6e89f94d2e
Jorge Neto <neto_tunas@hotmail.com>
2024-11-26 09:37:53 -0300

    fix: changed libraries that caused conflicts

--- babel.config.js
+++ babel.config.js
@@ -5,8 +5,12 @@
 //   };
 // };
 
-
 module.exports = {
   presets: ['module:metro-react-native-babel-preset'],
-  plugins: ['react-native-reanimated/plugin'],
+  plugins: [
+    ['react-native-reanimated/plugin', { loose: true }],
+    ['@babel/plugin-transform-class-properties', { loose: true }],
+    ['@babel/plugin-transform-private-methods', { loose: true }],
+    ['@babel/plugin-transform-private-property-in-object', { loose: true }],
+  ],
 };
